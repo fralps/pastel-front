8ce46848fdd2cba0faad68e5687799acb67f5ec6
fralps <francois@kinoba.fr>
2025-07-17 14:20:31 +0200

    chore: add initial Nuxt app structure with configuration and styles

---
+++ app/pages/index.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+  import { ref } from 'vue'
+
+  const value = ref('')
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center gap-4 h-screen">
+    <h1 class="font-bold text-2xl text-(--ui-primary)">
+      Nuxt UI - Starter
+    </h1>
+
+    <UInput v-model="value" />
+
+    <div class="flex items-center gap-2">
+      <UButton
+        label="Documentation"
+        icon="i-lucide-square-play"
+        to="https://ui.nuxt.com/getting-started/installation/nuxt"
+        target="_blank"
+      />
+
+      <UButton
+        label="GitHub"
+        color="neutral"
+        variant="outline"
+        icon="i-simple-icons-github"
+        to="https://github.com/nuxt/ui"
+        target="_blank"
+      />
+    </div>
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui'],
+  css: ['~/assets/css/main.css'],
   devServer: {
     port: 8080,
   }
