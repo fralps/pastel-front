5b376b9748500d5293c53178e9f45b3b769382e1
fralps <francois@kinoba.fr>
2025-09-16 15:16:22 +0200

    feat(i18n): add internationalization support with English and French locales
    - Created i18n configuration file to set fallback locale to English.
    - Added English and French locale JSON files with welcome and about messages.
    - Updated nuxt.config.ts to include @nuxtjs/i18n module and configured locales.
    - Added @nuxtjs/i18n dependency to package.json.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,16 +1,38 @@
 <script setup lang="ts">
   import { ref } from 'vue'
+  import { useI18n } from 'vue-i18n'
+  import type { SelectItem } from '@nuxt/ui'
 
-  const value = ref('')
+  const { locale, locales, setLocale } = useI18n({ useScope: 'global' })
+
+  const items = ref<SelectItem[]>(locales.value.map((locale) => ({
+    label: locale.name,
+    code: locale.code,
+    value: locale.code
+  })))
+  const currentLocale = ref(locale.value)
+
+  useSeoMeta({
+    title: 'Pastel - Accueil',
+    ogTitle: 'Pastel - Accueil',
+    description: 'Bienvenue sur Pastel, votre plateforme de création de contenu.',
+    ogDescription: 'Bienvenue sur Pastel, votre plateforme de création de contenu.',
+    ogImage: 'https://example.com/image.png',
+    twitterCard: 'summary_large_image',
+  })
 </script>
 
 <template>
   <div class="flex flex-col items-center justify-center gap-4 h-screen">
     <h1 class="font-bold text-2xl text-(--ui-primary)">
-      Nuxt UI - Starter
+      Pastel
     </h1>
 
-    <UInput v-model="value" />
+    {{ $t('welcome') }}
+
+    <USelect v-model="currentLocale" :items="items" @change="() => setLocale(currentLocale)" />
+
+    <NuxtLink to="/about">About</NuxtLink>
 
     <div class="flex items-center gap-2">
       <UButton
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,25 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/ui',
+    '@nuxtjs/i18n'
+  ],
+  i18n: {
+    strategy: 'prefix',
+    defaultLocale: 'en',
+    locales: [
+      { code: 'en', name: 'English', file: 'en.json' },
+      { code: 'fr', name: 'Français', file: 'fr.json' }
+    ],
+  },
   css: ['~/assets/css/main.css'],
   devServer: {
     port: 8080,
+  },
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' }
   }
 })
