47d9440f149ffc80ef39ea45d13974e91ce503e5
fralps <francois@kinoba.fr>
2025-09-16 16:11:21 +0200

    feat(ui): enhance navigation with dynamic links and improved layout

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
   import { ref } from 'vue'
   import { useI18n } from 'vue-i18n'
-  import type { SelectItem } from '@nuxt/ui'
+  import type { SelectItem, NavigationMenuItem  } from '@nuxt/ui'
 
+  
   const { locale, locales, setLocale } = useI18n({ useScope: 'global' })
 
   const items = ref<SelectItem[]>(locales.value.map((locale) => ({
@@ -12,6 +13,22 @@
   })))
   const currentLocale = ref(locale.value)
 
+  const links = ref([
+    {
+      label: 'Documentation',
+      to: 'https://ui.nuxt.com/getting-started/installation/nuxt',
+      color: 'neutral',
+      icon: 'i-lucide-square-play'
+    },
+    {
+      label: 'Github',
+      to: 'https://github.com/nuxt/ui',
+      color: 'neutral',
+      variant: 'outline',
+      trailingIcon: 'i-simple-icons-github'
+    }
+  ])
+
   useSeoMeta({
     title: 'Pastel - Accueil',
     ogTitle: 'Pastel - Accueil',
@@ -20,36 +37,68 @@
     ogImage: 'https://example.com/image.png',
     twitterCard: 'summary_large_image',
   })
+
+const route = useRoute()
+
+const navLinks = computed<NavigationMenuItem[]>(() => [
+  {
+    label: 'Docs',
+    to: '/docs/getting-started',
+    active: route.path.startsWith('/docs/getting-started')
+  },
+  {
+    label: 'Components',
+    to: '/docs/components',
+    active: route.path.startsWith('/docs/components')
+  },
+  {
+    label: 'Figma',
+    to: 'https://go.nuxt.com/figma-ui',
+    target: '_blank'
+  },
+  {
+    label: 'Releases',
+    to: 'https://github.com/nuxt/ui/releases',
+    target: '_blank'
+  }
+])
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center gap-4 h-screen">
-    <h1 class="font-bold text-2xl text-(--ui-primary)">
-      Pastel
-    </h1>
+  <div>
+    <UHeader>
+      <template #title>
+        <Logo class="h-6 w-auto" />
+      </template>
+
+      <UNavigationMenu :items="navLinks" />
+
+      <template #right>
+        <UColorModeButton />
+
+        <UTooltip text="Open on GitHub" :kbds="['meta', 'G']">
+          <UButton
+            color="neutral"
+            variant="ghost"
+            to="https://github.com/nuxt/ui"
+            target="_blank"
+            icon="i-simple-icons-github"
+            aria-label="GitHub"
+          />
+        </UTooltip>
+      </template>
+    </UHeader>
+    <UPageHero
+      title="Pastel"
+      description="Create and keep your dreams alive."
+      :links="links"
+      headline="The ultimate dreams diary platform"
+    />
 
     {{ $t('welcome') }}
 
-    <USelect v-model="currentLocale" :items="items" @change="() => setLocale(currentLocale)" />
+      <USelect v-model="currentLocale" :items="items" @change="() => setLocale(currentLocale)" />
 
     <NuxtLink to="/about">About</NuxtLink>
-
-    <div class="flex items-center gap-2">
-      <UButton
-        label="Documentation"
-        icon="i-lucide-square-play"
-        to="https://ui.nuxt.com/getting-started/installation/nuxt"
-        target="_blank"
-      />
-
-      <UButton
-        label="GitHub"
-        color="neutral"
-        variant="outline"
-        icon="i-simple-icons-github"
-        to="https://github.com/nuxt/ui"
-        target="_blank"
-      />
-    </div>
   </div>
 </template>
