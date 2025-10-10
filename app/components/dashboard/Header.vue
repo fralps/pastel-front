e4968170315b511067005c10809c2852ba10e514
fralps <francois@kinoba.fr>
2025-10-10 15:46:49 +0200

    feat: implement logout functionality in Header component

--- app/components/dashboard/Header.vue
+++ app/components/dashboard/Header.vue
@@ -1,14 +1,40 @@
+<script setup lang="ts">
+import { useCurrentUserStore } from '@/stores/currentUser'
+
+const currentUser = useCurrentUserStore()
+
+const logout = async (): Promise<void> => {
+  const userAuth = useCookie('token')
+
+  await useCustomFetch('/users/sign_out', {
+    method: 'DELETE'
+  })
+  userAuth.value = null
+  currentUser.$reset()
+  await useRouter().push('/auth/sign-in')
+}
+</script>
+
 <template>
   <UHeader :toggle="false">
     <template #title>
       <span class="text-primary text-2xl">Pastel</span>
     </template>
 
-
     <template #right>
       <UColorModeButton />
 
       <LocaleSelect />
+      <UButton
+        icon="i-lucide-log-out"
+        size="sm"
+        color="error"
+        variant="soft"
+        class="text-xs cursor-pointer"
+        @click="logout()"
+      >
+        Log Out
+      </UButton>
     </template>
   </UHeader>
 </template>
