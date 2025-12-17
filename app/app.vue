773651b5b57a5d68c2a36c5cb0214d2215c7c025
fralps <francois@kinoba.fr>
2025-12-17 15:30:44 +0100

    feat: add umami script

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,17 @@
+<script setup lang="ts">
+if (import.meta.env.VITE_APP_ENV === 'production') {
+  useHead({
+    script: [
+      {
+        src: 'https://cloud.umami.is/script.js',
+        defer: true,
+        'data-website-id': 'f3962bc5-40e8-4ad0-bd49-4696276dd5fb'
+      }
+    ],
+  })
+}
+</script>
+
 <template>
   <UApp>
     <NuxtPage />
